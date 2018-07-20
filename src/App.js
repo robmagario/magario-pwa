39bbc919e276e23a17528e757cbd9803125e1b25
Hemant <hemantsureshkumar2012@gmail.com>
2018-07-20 17:46:59 +0800

    added basic appear animations to everything

--- src/App.js
+++ src/App.js
@@ -26,11 +26,7 @@ var deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', function (e) {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-
-    this.handleClick();
+    alert("hello");
 
 });
 
