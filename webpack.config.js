5c829e18d5616e0d2aef19b63d3c700e45c80807
robmagario <1ronison>
2018-07-30 17:57:00 +0800

    This is the newest version that includeds WorkBox.

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,3 @@ module.exports = {
     ]
 };
 
-// Inside of webpack.config.js:
-const {GenerateSW} = require('workbox-webpack-plugin');
-
-module.exports = {
-    // Other webpack config...
-    plugins: [
-        // Other plugins...
-        new GenerateSW()
-    ]
-};
